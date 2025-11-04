d0bd66f66b890770cc5605c9f6b7144c080d84e7
Edwin Fischer <info@edwinfischer.com>
2025-11-04 17:21:53 +0100

    Dekstop Aufgabe 1-3fertig

---
+++ asteroids/CMakeFiles/main_game.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ede/Documents/WS_ComputerGrafik/asteroids/game.cc" "CMakeFiles/main_game.dir/game.cc.o" "gcc" "CMakeFiles/main_game.dir/game.cc.o.d"
+  "/home/ede/Documents/WS_ComputerGrafik/asteroids/geometry.cc" "CMakeFiles/main_game.dir/geometry.cc.o" "gcc" "CMakeFiles/main_game.dir/geometry.cc.o.d"
+  "/home/ede/Documents/WS_ComputerGrafik/asteroids/main_game.cc" "CMakeFiles/main_game.dir/main_game.cc.o" "gcc" "CMakeFiles/main_game.dir/main_game.cc.o.d"
+  "/home/ede/Documents/WS_ComputerGrafik/asteroids/math.cc" "CMakeFiles/main_game.dir/math.cc.o" "gcc" "CMakeFiles/main_game.dir/math.cc.o.d"
+  "/home/ede/Documents/WS_ComputerGrafik/asteroids/physics.cc" "CMakeFiles/main_game.dir/physics.cc.o" "gcc" "CMakeFiles/main_game.dir/physics.cc.o.d"
+  "/home/ede/Documents/WS_ComputerGrafik/asteroids/sdl2_game_controller.cc" "CMakeFiles/main_game.dir/sdl2_game_controller.cc.o" "gcc" "CMakeFiles/main_game.dir/sdl2_game_controller.cc.o.d"
+  "/home/ede/Documents/WS_ComputerGrafik/asteroids/sdl2_renderer.cc" "CMakeFiles/main_game.dir/sdl2_renderer.cc.o" "gcc" "CMakeFiles/main_game.dir/sdl2_renderer.cc.o.d"
+  "/home/ede/Documents/WS_ComputerGrafik/asteroids/sound.cc" "CMakeFiles/main_game.dir/sound.cc.o" "gcc" "CMakeFiles/main_game.dir/sound.cc.o.d"
+  "/home/ede/Documents/WS_ComputerGrafik/asteroids/timer.cc" "CMakeFiles/main_game.dir/timer.cc.o" "gcc" "CMakeFiles/main_game.dir/timer.cc.o.d"
+  "" "main_game" "gcc" "CMakeFiles/main_game.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ asteroids/CMakeFiles/physics_test.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ede/Documents/WS_ComputerGrafik/asteroids/geometry.cc" "CMakeFiles/physics_test.dir/geometry.cc.o" "gcc" "CMakeFiles/physics_test.dir/geometry.cc.o.d"
+  "/home/ede/Documents/WS_ComputerGrafik/asteroids/math.cc" "CMakeFiles/physics_test.dir/math.cc.o" "gcc" "CMakeFiles/physics_test.dir/math.cc.o.d"
+  "/home/ede/Documents/WS_ComputerGrafik/asteroids/physics.cc" "CMakeFiles/physics_test.dir/physics.cc.o" "gcc" "CMakeFiles/physics_test.dir/physics.cc.o.d"
+  "/home/ede/Documents/WS_ComputerGrafik/asteroids/physics_test.cc" "CMakeFiles/physics_test.dir/physics_test.cc.o" "gcc" "CMakeFiles/physics_test.dir/physics_test.cc.o.d"
+  "/home/ede/Documents/WS_ComputerGrafik/asteroids/timer.cc" "CMakeFiles/physics_test.dir/timer.cc.o" "gcc" "CMakeFiles/physics_test.dir/timer.cc.o.d"
+  "" "physics_test" "gcc" "CMakeFiles/physics_test.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
