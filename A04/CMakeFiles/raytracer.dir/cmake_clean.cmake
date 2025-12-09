825ff39354290b6e6be43a213631ff981dd215fd
Edwin Fischer <info@edwinfischer.com>
2025-12-09 23:26:07 +0100

    a4 Abgabe

---
+++ A04/CMakeFiles/raytracer.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raytracer.dir/link.d"
+  "CMakeFiles/raytracer.dir/geometry.cc.o"
+  "CMakeFiles/raytracer.dir/geometry.cc.o.d"
+  "CMakeFiles/raytracer.dir/math.cc.o"
+  "CMakeFiles/raytracer.dir/math.cc.o.d"
+  "CMakeFiles/raytracer.dir/raytracer.cc.o"
+  "CMakeFiles/raytracer.dir/raytracer.cc.o.d"
+  "raytracer"
+  "raytracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raytracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
